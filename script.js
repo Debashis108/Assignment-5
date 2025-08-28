35b6209cb9e1604e7699aa3ce64b0590ef7e3fb0
Debashis108 <deba.barmon@gmail.com>
2025-08-29 01:02:20 +0600

    Alert message functionality done

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 // Heart Icons
-let heartCount = 0;
-let hearts = document.getElementsByClassName("heart-icon-class");
-for (let heart of hearts) {
-  heart.addEventListener("click", function () {
-    heartCount++;
-    document.getElementById("heart-count-id").innerText = heartCount;
-  });
-}
+// let heartCount = 0;
+// let hearts = document.getElementsByClassName("heart-icon-class");
+// for (let heart of hearts) {
+//   heart.addEventListener("click", function () {
+//     heartCount++;
+//     document.getElementById("heart-count-id").innerText = heartCount;
+//   });
+// }
+
+// alert message
+// const serviceNames = document.getElementsByClassName("service-name-class");
+// const serviceNumbers = document.getElementsByClassName("service-number-class");
+// const callButtons = document.getElementsByClassName("call-btn");
+
+// for (let i = 0; i < callButtons.length; i++) {
+//   callButtons[i].addEventListener("click", function () {
+//     let serviceName = serviceNames[i].innerText;
+//     let serviceNumber = serviceNumbers[i].innerText;
+//     alert(`Calling ${serviceName} (${serviceNumber})`);
+//   });
+// }
