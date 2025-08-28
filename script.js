903f1ab43053b098e1251a2684da492ca6227521
Debashis108 <deba.barmon@gmail.com>
2025-08-29 02:28:34 +0600

    History appearance in call history

--- script.js
+++ script.js
@@ -33,18 +33,35 @@
 //   });
 // }
 
-let coins = 100;
-let coinCount = document.getElementById("coin-count");
+// coins alert message
+// let coins = 100;
+// // let coinCount = document.getElementById("coin-count");
+// // const callButtons = document.getElementsByClassName("call-btn");
+
+// // for (let i = 0; i < callButtons.length; i++) {
+// //   callButtons[i].addEventListener("click", function () {
+// //     coins = coins - 20;
+// //     coinCount.innerText = coins;
+// //     if (coins < 20) {
+// //       alert("Not enough coins");
+// //       coins = 100;
+// //       return;
+// //     }
+// //   });
+// }
+
+// list of called history
+const serviceNames = document.getElementsByClassName("service-name-class");
+const serviceNumbers = document.getElementsByClassName("service-number-class");
 const callButtons = document.getElementsByClassName("call-btn");
+let callHistory = document.getElementById("call-history-id");
 
 for (let i = 0; i < callButtons.length; i++) {
   callButtons[i].addEventListener("click", function () {
-    coins = coins - 20;
-    coinCount.innerText = coins;
-    if (coins < 20) {
-      alert("Not enough coins");
-      coins = 100;
-      return;
-    }
+    let serviceName = serviceNames[i].innerText;
+    let serviceNumber = serviceNumbers[i].innerText;
+    let li = document.createElement("li");
+    li.innerText = `${serviceName}-${serviceNumber}`;
+    callHistory.appendChild(li);
   });
 }
